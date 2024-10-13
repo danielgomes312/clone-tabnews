98fa2117d22cefd119a815e62a908d351c77385a
Daniel Gomes de Andrade <83084079+danielgomes312@users.noreply.github.com>
2024-10-13 14:26:57 +0000

    modificar a frase

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
     return <div>
         <h1>Just to me, I will not gonna show this to anyone I am happy to know that I know english so I will let just that.</h1>
         <h2>what happen if I do that</h2>
+        <h2>if I make ane change here it's gonna be a big ona</h2>
     </div>
 }
 
