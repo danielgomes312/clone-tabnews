d70bc3d6ee5fa411430d72cbb2a41cab0873bd48
Daniel Gomes de Andrade <83084079+danielgomes312@users.noreply.github.com>
2024-10-13 04:04:18 +0000

    Changing this again just to have sure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Just to me, I will not gonna show this to anyone I am happy to know that I know english so I will let just that.</h1>
+    return <div>
+        <h1>Just to me, I will not gonna show this to anyone I am happy to know that I know english so I will let just that.</h1>
+        <h2>what happen if I do that</h2>
+    </div>
 }
 
 export default Home;
