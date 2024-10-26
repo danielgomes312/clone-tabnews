75a1221cdd7a1710c5aceffe6d7c59e2598a13ca
Daniel Gomes de Andrade <83084079+danielgomes312@users.noreply.github.com>
2024-10-26 02:13:34 +0000

    just another error test

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ function Home() {
     return <div>
         <h1>Just to me, I will not gonna show this to anyone I am happy to know that I know english so I will let just that.</h1>
         <h2>what happen if I do that</h2>
+        <h2>I kind forget to put the ammend</h2>
+        <h2>Lets add something more to teste the error mesenger</h2>
         <h2>if I make ane change here it's gonna be a big ona</h2>
     </div>
 }
