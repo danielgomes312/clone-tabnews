e022ca5db47ba100b92ead844ffa82a45460fcef
Daniel Gomes de Andrade <83084079+danielgomes312@users.noreply.github.com>
2024-12-05 00:13:40 +0000

    just making a style teste

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home() {
   return (
     <div>
-      <h1 style="color:blue;">
+      <h1>
         Just to me, I will not gonna show this to anyone I am happy to know that
         I know english so I will let just that. Let's see if works
       </h1>
-      <h2>what happen if I do that</h2>
+      <h2>Maninha feia</h2>
       <h2>I kind forget to put the ammend</h2>
       <h2>Lets add something more to teste the error mesenger</h2>
       <h2>if I make ane change here it's gonna be a big ona</h2>
