ebbfdcfef635384024bfa66e09082468204776db
Daniel Gomes de Andrade <83084079+danielgomes312@users.noreply.github.com>
2024-12-21 19:09:30 +0000

    Stylish mistaken corrected

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1 style="color: red;">
+      <h1 style={{ color: "red" }}>
         Just to me, I will not gonna show this to anyone I am happy to know that
         I know english so I will let just that. Let's see if works
       </h1>
