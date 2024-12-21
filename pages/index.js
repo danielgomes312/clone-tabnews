5c540c379cd5887e5d77e240564c15f77609cd44
Daniel Gomes de Andrade <83084079+danielgomes312@users.noreply.github.com>
2024-12-21 19:01:14 +0000

    just making some visual tests

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 style="color: red;">
         Just to me, I will not gonna show this to anyone I am happy to know that
         I know english so I will let just that. Let's see if works
       </h1>
-      <h2>Maninha feia</h2>
-      <h2>I kind forget to put the ammend</h2>
-      <h2>Lets add something more to teste the error mesenger</h2>
-      <h2>if I make ane change here it's gonna be a big ona</h2>
-      <h2>Just test again</h2>
     </div>
   );
 }
